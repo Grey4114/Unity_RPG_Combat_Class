af7dfd76b03954c255b4df130a76134fcdfd02e0
Grey4114 <60712674+Grey4114@users.noreply.github.com>
2021-12-14 10:15:46 -0800

    Character Update
    Added Character, Animations, movements and camera follow

---
+++ RPG_Combat_Class/Assets/Scripts/Mover.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    // Ray lastRay;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+
+            // Raycast: Gets the from the camera to the mouse click position
+            // lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        // Raycast: This will show the raycast line in Scene window during Play
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
+        // GetComponent<NavMeshAgent>().destination = target.position;
+
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // RayCast: 
+        // Must have the keyword "out" in front of hit
+        bool hasHit = Physics.Raycast(ray, out hit); 
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
