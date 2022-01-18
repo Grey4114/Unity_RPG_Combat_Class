7c366c5a6cba1fe9ecbf96e091a08db7c75bfe65
Grey4114 <grey4114@yahoo.com>
2022-01-18 10:38:04 -0800

    Scene Management Coroutines

--- RPG_Combat_Class/Assets/Scripts/Cinematics/CinematicControlRemover.cs
+++ RPG_Combat_Class/Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
@@ -11,25 +11,24 @@ namespace RPG.Cinematics
 
     public class CinematicControlRemover : MonoBehaviour
     {
+
         GameObject player;
 
         private void Start() 
         {
-
+            player = GameObject.FindWithTag("Player");
             GetComponent<PlayableDirector>().played += DisableControl;
             GetComponent<PlayableDirector>().stopped += EnableControl;
         }
 
         void DisableControl(PlayableDirector aDirector)
         {
-            player = GameObject.FindWithTag("Player");
             player.GetComponent<ActionScheduler>().CancelCurrentAction();
             player.GetComponent<PlayerController>().enabled = false;
         }
 
         void EnableControl(PlayableDirector aDirector)
         {
-            // print("Enabled");
             player.GetComponent<PlayerController>().enabled = true;
         }
 
