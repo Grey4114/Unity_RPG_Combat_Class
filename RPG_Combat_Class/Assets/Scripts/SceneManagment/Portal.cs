b62736848d1c8608ff7bf19ce162eb9fcacf9a93
Grey4114 <grey4114@yahoo.com>
2022-01-18 19:56:56 -0800

    Scene Management Portals

--- RPG_Combat_Class/Assets/Scripts/SceneManagment/Portal.cs
+++ RPG_Combat_Class/Assets/Scripts/SceneManagment/Portal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,6 +10,7 @@ namespace RPG.SceneManagment
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other) 
         {
@@ -19,13 +21,40 @@ namespace RPG.SceneManagment
 
         }
 
+
         private IEnumerator Transition()
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("Scene Loaded");
+            
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+
             Destroy(gameObject);
         }
+
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
 
 
     }
