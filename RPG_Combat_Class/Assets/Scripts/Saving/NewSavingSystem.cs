8f15641092661382e99fb927974247b31ab40170
Grey4114 <grey4114@yahoo.com>
2022-01-25 09:39:09 -0800

    Save System - Vector3 Save Load

--- RPG_Combat_Class/Assets/Scripts/Saving/NewSavingSystem.cs
+++ RPG_Combat_Class/Assets/Scripts/Saving/NewSavingSystem.cs
@@ -6,7 +6,7 @@ using System.Text;
 using UnityEngine;
 
 
-// This script controls all of the saving/loading to the save files
+// This script controls all of the saving/loading for the save files
 namespace RPG.Saving
 {
     public class NewSavingSystem : MonoBehaviour 
@@ -24,10 +24,13 @@ namespace RPG.Saving
             // Once the Using statement is exited then the stream in closed automatically
             using (FileStream stream = File.Open(path, FileMode.Create))
             {
-                byte[] bytes = Encoding.UTF8.GetBytes("!Hola Mundo!");
-                stream.Write(bytes, 0, bytes.Length);
+                Transform playerTransform = GetPlayerTransform();  // get player position
+                byte[] buffer = SerializeVector(playerTransform.position); // convert to byte array
+                stream.Write(buffer, 0, buffer.Length); // write to save file
             }
         }
+
+
 
 
         //Loads all of the info to the file
@@ -38,14 +41,45 @@ namespace RPG.Saving
 
             using (FileStream stream = new FileStream(path, FileMode.Open))
             {
-                byte[] buffer = new byte[stream.Length];
-                stream.Read(buffer, 0, buffer.Length);
-                print(Encoding.UTF8.GetString(buffer));
+
+                byte[] buffer = new byte[stream.Length];  // create empty byte array based on save file size 
+                stream.Read(buffer, 0, buffer.Length);  // fill the buffer with the save file info
+                Transform playerTransform = GetPlayerTransform();  // gets players current position
+                playerTransform.position = DeserializeVector(buffer);  // Converts to a vector3 & loads new position
+                
             }
 
         }
 
 
+        // Get the players vectore3 position
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
+        }
+
+        // Convert the vector3 into an array of bytes
+        private byte[] SerializeVector(Vector3 vector)
+        {
+            byte[] vectorBytes = new byte[3 * 4];
+            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes,0);
+            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes,4);
+            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes,8);
+            return vectorBytes;
+        }
+
+        // Convert the array of bytes into a vector3
+        private Vector3 DeserializeVector(byte[] buffer)
+        {
+            Vector3 result = new Vector3();
+            result.x = BitConverter.ToSingle(buffer,0);
+            result.y = BitConverter.ToSingle(buffer,4);
+            result.z = BitConverter.ToSingle(buffer,8);
+            return result;
+        }
+
+
+        // Returns the systems current default path/location for save files
         private string GetPathFromSaveFile(string saveFile)
         {
             // Saves the file to the system default location 
